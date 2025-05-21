6abbb9c0b57919a3a42f4c1f202fa1d92eeb4cb3
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-21 13:18:56 +0200

    finished my first

--- src/main/java/com/example/hellospring/HelloController.java
+++ src/main/java/com/example/hellospring/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello, Spring Boot!";
+        return "Hello, Spring Boot, I am running!";
     }
 }
