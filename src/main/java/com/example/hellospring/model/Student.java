b79fa372f5b8fbdfe22e0dba87e68fa5b1b6f1c3
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-21 15:04:23 +0200

    finished the string making

--- src/main/java/com/example/hellospring/model/Student.java
+++ src/main/java/com/example/hellospring/model/Student.java
@@ -5,5 +5,12 @@ import jakarta.persistence.*;
 
 
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String course;
     
 }
